62cd97bb6ede3c126c73bb335216d0b8079a4f05
josemsb92 <josemsb92@gmail.com>
2022-04-15 00:03:12 +0100

    useEffect to get url info WIP

--- src/views/externalUrl.jsx
+++ src/views/externalUrl.jsx
@@ -1,8 +1,22 @@
+import {useState, useEffect} from "react"
+import axios from "axios"
 function ExternalUrl() {
-  const urlRedirect = ""; //Necesito hacer un getUrl con req.param y cortar por el codigo para obtener el link
+  const [urlRedirect, setUrlRedirect] = useState("") //Necesito hacer un getUrl con req.param y cortar por el codigo para obtener el link
+  const shortUrl = window.location.href.split("/urlchopper/")
+
+  useEffect(() =>{
+    const OriginalUrl = async () => {
+       await axios.get(`localhost:3003/urlchopper/url/nH1Mt5`).then((response) => console.log(response))
+    }
+    OriginalUrl()
+
+
+  },[])
+  
   return (
     <div className="test-view">
-      {(window.location.href = urlRedirect)}</div>
+      {/* {(window.location.href = urlRedirect)} */}
+    </div>
   );
 }
 export default ExternalUrl;
