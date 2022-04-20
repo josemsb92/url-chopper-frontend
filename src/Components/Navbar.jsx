66f423173abd1f108207f1c3d469eca2eb470f15
josemsb92 <josemsb92@gmail.com>
2022-04-20 22:46:20 +0200

    Navbar formatter to prettier

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Portal from '@mui/material/Portal';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Portal from "@mui/material/Portal";
 
 export default function ButtonAppBar() {
-    const [show, setShow] = React.useState(false);
+  const [show, setShow] = React.useState(false);
   const container = React.useRef(null);
 
   const handleClick = () => {
@@ -18,49 +18,36 @@ export default function ButtonAppBar() {
   };
 
   return (
-      <div className="navbar-body">
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Links-Chopper
-          </Typography>
-          <Button onClick={handleClick} color="inherit"><AccountCircleIcon fontSize="large"/></Button>
-        </Toolbar>
-      </AppBar>
+    <div className="navbar-body">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Links-Chopper
+            </Typography>
+            <Button onClick={handleClick} color="inherit">
+              <AccountCircleIcon fontSize="large" />
+            </Button>
+          </Toolbar>
+        </AppBar>
 
-     
         {show ? (
-           
           <Portal container={container.current}>
-               <div className='login-portal'>
-            <span>Formulario</span>   
-            </div>         
+            <div className="login-portal">
+              <span>Formulario</span>
+            </div>
           </Portal>
-         
         ) : null}
-     
-      
-
-
-
-    </Box> 
-
+      </Box>
     </div>
-      
-    
-  
-
-
-
   );
 }
